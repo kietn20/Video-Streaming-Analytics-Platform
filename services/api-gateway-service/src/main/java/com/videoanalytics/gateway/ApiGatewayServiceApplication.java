9b5ede21cbbbfe7cde7f86987964dee145927798
Kiet Nguyen <kietnguyen3698@gmail.com>
2025-02-09 19:57:01 -0800

    working on service stubs

---
+++ services/api-gateway-service/src/main/java/com/videoanalytics/gateway/ApiGatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package com.videoanalytics.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point for the API Gateway Service
+ * File: src/main/java/com/videoanalytics/gateway/ApiGatewayApplication.java
+ */
+@SpringBootApplication
+public class ApiGatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
